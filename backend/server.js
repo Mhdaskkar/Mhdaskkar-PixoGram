d3e20c5a9ddf47ae9361727db94b7762aa064244
Mhdaskkar <Muhammed_Askkar-MA@ulster.ac.uk>
2026-05-03 00:50:03 +0100

    fix: node20 + crypto polyfill for cosmos sdk

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-﻿// Deploy: 2026-05-03 00:09:50
+﻿if (typeof globalThis.crypto === 'undefined') { globalThis.crypto = require('crypto'); }
+// Deploy: 2026-05-03 00:09:50
 /**
  * PixoGram API â€” Express Server Entry Point
  * Azure Cloud-Native Photo Sharing Platform
@@ -147,6 +148,7 @@ if (require.main === module) {
 }
 
 module.exports = app;
+
 
 
 
